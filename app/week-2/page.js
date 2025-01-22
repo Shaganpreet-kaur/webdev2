6fe4d365079b7309426a88c45690bbc1a41dce86
Shaganpreet-kaur <shaganpreetk632@gmail.com>
2025-01-22 11:39:28 -0700

    added title component

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+export default function Page(){
+    let a=5;
+    let b=6;
+    return(
+        <main>
+            <h1>Week 2</h1>
+            <p> this is a simple web page</p>
+            <p>sum of {a} and{b} os {a+b}</p>
+        </main>
+    )
+}
